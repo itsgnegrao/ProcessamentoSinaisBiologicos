2c42a191eb7c13b53db8385ab6190820cdfe7af0
ItsgNegrao <silvag.1996@alunos.utfpr.edu.br>
2019-07-07 23:31:57 -0300

    finish him

--- script.py
+++ script.py
@@ -1,90 +1,98 @@
 import copy
 import sys
-from re import search
 from subprocess import getoutput as gop
 
 import matplotlib.pyplot as plt
 import mne
 import numpy as np
-
+from mne.time_frequency import psd_welch as psdw
 
 def loadData(arquivo):
     arqOpen = open(f'data_10hz/{arquivo}')
+    fileLimitTime = 0
     
     for _ in range(6):
         next(arqOpen)
     
-    person = list()
     eeg = list()
+    person = list()
     for row in arqOpen:
-        columns = row.replace('\n','').split(',')
+        columns = row.split(',')
 
         if columns:
-            columns = [float(i) for i in columns[:11]]
+            columns = columns[:7]
                     
-            if(columns[0] == 0):
+            if(columns[0] == '0'):
                 if(len(eeg) == 256):
-                    person.append(copy.deepcopy(eeg))
+                    fileLimitTime +=1
+                    for k in eeg:
+                        person.append(list(map(lambda i : float(i), k[1:])))
                 eeg = list()
-                
-            columns = columns[1:]    
-                
-            # eeg.append(np.array(columns).reshape(10,1))
+            
             eeg.append(columns)
 
-    return np.array(person)
+    return [np.array(person).T, fileLimitTime]
 
-def reshapeData(data):
-    newData = list()
-    for d in data:
-        row = list()
-        for j in range(0,10):
-            values = list()
-            for i in d:
-                values.append(i[j])
-            row.append(copy.deepcopy(values))
-        newData.append(copy.deepcopy(row))
+def prepareRaw(data):
+    info = mne.create_info(
+        ch_names=['eeg'] * 6, 
+        sfreq=256, 
+        ch_types=['eeg'] * 6)
+
+    raw = mne.io.RawArray(data, info)
+
+    for _ in range(5):
+        raw.filter(l_freq=5, h_freq=50)
+
+    return raw
+
+
+def scaleValue(max, min):
+    return ((min*100)/max)
+
+def findAlphaTime(raw, tmin, window):
+    faixas = list()
     
-    return np.array(newData)
+    alpha = psdw(raw, tmin=tmin, tmax=tmin+window, fmin=8, fmax=12, verbose=False)[0]   
+    theta = psdw(raw, tmin=tmin, tmax=tmin+window, fmin=5, fmax=7, verbose=False)[0]
+    beta = psdw(raw, tmin=tmin, tmax=tmin+window, fmin=12, fmax=30, verbose=False)[0]
+    gamma = psdw(raw, tmin=tmin, tmax=tmin+window, fmin=25, fmax=100, verbose=False)[0]
+    
+    faixas.append(np.average(alpha))
+    faixas.append(np.average(theta))
+    faixas.append(np.average(beta))
+    faixas.append(np.average(gamma))
+    
+    indexMaiorFaixa = np.argmax(np.array(faixas))
+    
+    if (indexMaiorFaixa == 0):
+        indexSegMaiorFaixa = np.argmax(np.array(faixas[1:]))+1
+        print(f'{tmin}: {scaleValue(faixas[indexMaiorFaixa], faixas[indexSegMaiorFaixa])}')
 
-
+def plotWindowTime(raw, tmin, window, fmax=30):
+    psds, freqs = psdw(raw, tmin=tmin, tmax=tmin+window, fmax=fmax)
+    psd_Average = np.average(psds, axis=0) ** 2
+    plt.plot(freqs, psd_Average)
+    plt.show()
                     
 def main():
     # arquivos
     arquivos = gop('ls data_10hz').split('\n')
-   
-    # (234, 10, 256)  reshapaduuuu
-    # 
-    # # for arq in arquivos:
-    # d1 = reshapeData(loadData(arquivos[2]))
-    # print(d1.shape)
+    times = [[95, 96, 234, 367, 597, 786],
+            [39, 46, 110, 130, 280, 363],
+            [103, 114, 136, 154, 171, 185]]
+    window = 3
+    
+    for arqPos, arq in enumerate(arquivos):
+        data, fileLimitTime = loadData(arq)
+        print(data.shape)
+        raw = prepareRaw(data)
 
-    # d1 = d1[0]
+        # for i in range(fileLimitTime - window):
+        #     findAlphaTime(raw, i, window)
 
-    # ch_names = ['1','2','3','4','5','6','7','8','9','10']
-    # ch_types = ['eeg'] * 10
-
-    # info = mne.create_info(
-    #     ch_names=ch_names, 
-    #     sfreq=256, 
-    #     ch_types=ch_types)
-
-    # print(info)
-    # print(d1.shape)
-
-    # raw = mne.io.RawArray(d1, info)
-
-    # # raw.drop_channels(['x','nd','y'])
-    # montage = mne.channels.read_montage('standard_1020')
-    # raw.set_montage(montage)
-    # raw.plot_psd()
-    # print()
-
-    # # Grafico no domínio da frequencia
-    # plt.plot(np.linspace(0,1,256), raw.get_data()[0])
-    # plt.xlabel('tempo (s)')
-    # plt.ylabel('Dados EEG (mV/cm²)')
-
+        for i in times[arqPos]:
+            plotWindowTime(raw, i, window)
 
 if __name__ == '__main__':
     main()
