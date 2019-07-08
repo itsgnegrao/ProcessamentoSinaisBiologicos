1e0f17625141900464cb7208ac9bd0e7c59a301c
itsg_negrao <itsg_negrao@hotmail.com>
2019-07-07 23:33:12 -0300

    finish him2

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-      columns = row.split(',')
-            # print(columns[0])
-            # print(columns[12])
-            print(len(columns))
-            for k in columns:
-                print(k)
-            exit(0)
